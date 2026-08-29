3cb50958ad638cce8aa35b4ef16edef5cc0ef995
Peter Tecnet <petertecnet@gmail.com>
2026-08-29 14:44:14 -0300

    Identify Inkap in API requests

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,25 @@
 import axios from "axios";
 import { apiBaseUrl } from "../config";
 
+const APP_SLUG = "inkap";
+
 const api = axios.create({
   baseURL: apiBaseUrl,
+  timeout: 20000,
+  headers: {
+    Accept: "application/json",
+    "X-Peter-App": APP_SLUG,
+  },
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  config.headers = config.headers || {};
+  config.headers["X-Peter-App"] = APP_SLUG;
+  if (token && !config.headers.Authorization) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 
 export default api;
