1ca174118220f1911c50c34480a0a014e5cc5419
Peter Tecnet <petertecnet@gmail.com>
2026-08-29 14:44:44 -0300

    Sign direct Inkap axios requests

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import axios from "axios";
 import App from "./App";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import "bootstrap/dist/css/bootstrap.min.css";
+
+axios.defaults.headers.common["X-Peter-App"] = "inkap";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
