44bfbf3ca8f5276a070ddb3034b06b49436b35a3
Peter Tecnet <petertecnet@gmail.com>
2026-08-30 15:41:19 -0300

    fix: declare application id in telemetry

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { startTelemetry } from "./telemetry";
-import { apiBaseUrl, appSlug } from "./config";
+import { apiBaseUrl, appId, appSlug } from "./config";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import axios from "axios";
@@ -7,7 +7,7 @@ import App from "./App";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-startTelemetry({ apiBaseUrl, appSlug });
+startTelemetry({ apiBaseUrl, appSlug, appId });
 
 axios.defaults.headers.common["X-Peter-App"] = "inkap";
 
