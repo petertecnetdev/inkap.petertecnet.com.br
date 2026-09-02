29eed93fa94ac1a2ede5a4eca8addf5e37b51483
Peter Tecnet <petertecnet@gmail.com>
2026-09-02 11:55:54 -0300

    feat: habilita launcher do ecossistema no Inkap

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import axios from "axios";
 import App from "./App";
+import PeterAccountGateway from "./components/PeterAccountGateway";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -15,7 +16,9 @@ axios.defaults.headers.common["X-App-ID"] = String(appId);
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
-      <App />
+      <PeterAccountGateway apiBaseUrl={apiBaseUrl} appSlug={appSlug}>
+        <App />
+      </PeterAccountGateway>
     </GoogleOAuthProvider>
   </React.StrictMode>
 );
